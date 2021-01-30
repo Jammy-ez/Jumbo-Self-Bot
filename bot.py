cd8cb8c4b4a89a1416f8d9867bd2fb08d618ba63
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-30 14:26:03 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def clear(): #this clears screen
 clear()
 if token == '':
     token = input(MAGENTA + "Enter discord token: ")
-version = "1.0"
+version = "1.1"
 main = (MAGENTA + ''' 
 Made by jam
 
